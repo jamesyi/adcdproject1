319052ba2f654bcb07a710c7cb25f5a8ec93598d
cymuffin <clarkyao@Clarks-MacBook-Pro.local>
2014-01-27 16:16:24 -0800

    change the database name

--- project1/db/connectDB.php
+++ project1/db/connectDB.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "root", "Project1");
+$con = mysqli_connect("localhost", "root", "root", "Cosplayer");
 
 if(mysqli_connect_error()){
 	//failed to connect
